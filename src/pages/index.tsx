0a7044e77e2dff4cb447544061bdd90f12d009f7
Sara Rocha <sararocha@MacBook-Air-de-Codefique.local>
2022-03-14 19:51:55 -0300

    part1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton/';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+// Client-side
+// Server-side
+// Static Site Generation
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -16,13 +29,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get acess to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KdLM8IKLDBW16T1PCLHnvZi')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100), //divide por 100 pq chega como centavos
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
---
+++ src/services/stripe.ts
@@ -0,0 +1,15 @@
+//arquivo que vai definir a conexao com a api de pagamento STRIPE
+
+import Stripe from 'stripe';
+import { version } from '../../package.json';
+
+export const stripe = new Stripe(
+  process.env.STRIPE_API_KEY,
+  {
+    apiVersion: '2020-08-27',
+    appInfo: {
+      name: 'Ignews',
+      version
+    },
+  }
+);
