52525f34c6ad956e0679c36d31a7e60d375a1bde
Sara Rocha <sararocha@MacBook-Air-de-Codefique.local>
2022-03-28 19:38:07 -0300

    stripe concluido

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,18 +21,35 @@ export default NextAuth({
 
     async signIn(user, account, profile) {
       const { email } = user;
+
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
         return true;
       } catch (error) {
-        console.log("Error", error);
-        
+        // console.log("Error", error);
+
         return false;
 
       }
