bff225b2cded9bc38cd52566de8aa46a91eb259a
Sara Rocha <sararocha@MacBook-Air-de-Codefique.local>
2022-04-07 19:26:29 -0300

    Consumindo API do Prismic

--- .slicemachine/assets/slices/Publication/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Publication';
-
-export default {
-  title: 'slices/Publication'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"publication","items":[],"primary":{"title":[{"type":"heading1","text":"Exploit granular niches","spans":[]}],"description":[{"type":"paragraph","text":"Ipsum elit deserunt excepteur amet ullamco nostrud non exercitation veniam.","spans":[]}]},"id":"_Default"}} />
-_Default.storyName = 'Default'
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,9 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicCliente } from '../../services/prismic';
 import styles from './styles.module.scss';
+
+import Prismic from '@prismicio/client';
 
 export default function Posts() {
   return (
@@ -8,8 +12,8 @@ export default function Posts() {
         <title>Posts | IgNews</title>
       </Head>
 
-      <main>
-        <div>
+      <main className={styles.container}>
+        <div className={styles.posts}>
           <a href="">
             <time>06 de Abril de 2022</time>
             <strong>Aqui vai estar um titulo</strong>
@@ -29,4 +33,23 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicCliente()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  })
+
+  console.log(response);
+
+  return {
+    props: {
+
+    }
+  }
 }
